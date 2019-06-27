3b8c70bf152b356a72fad4c60bd8bf25e37de96c
headlulu888 <headlulu888@gmail.com>
2019-06-27 18:44:54 +0300

    add Post && Category observer

---
+++ app/Observers/BlogPostObserver.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\BlogPost;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+
+/**
+ * Class BlogPostObserver
+ * @package App\Observers
+ */
+class BlogPostObserver
+{
+    /**
+     * @param BlogPost $blogPost
+     */
+    public function creating(BlogPost $blogPost)
+    {
+
+    }
+
+    /**
+     * @param BlogPost $blogPost
+     */
+    public function updating(BlogPost $blogPost)
+    {
+        /*$test[] = $blogPost->isDirty();
+        $test[] = $blogPost->isDirty('is_published');
+        $test[] = $blogPost->isDirty('user_id');
+        $test[] = $blogPost->getAttribute('is_published');
+        $test[] = $blogPost->is_published;
+        $test[] = $blogPost->getOriginal('is_published');
+        dd($test);*/
+
+        $this->setPublishedAt($blogPost);
+        $this->setSlug($blogPost);
+    }
+
+    /**
+     * @param BlogPost $blogPost
+     */
+    protected function setPublishedAt(BlogPost $blogPost)
+    {
+        if (empty($blogPost->published_at) && $blogPost->is_published) {
+            $blogPost->published_at = Carbon::now();
+        }
+    }
+
+    /**
+     * @param BlogPost $blogPost
+     */
+    protected function setSlug(BlogPost $blogPost)
+    {
+        if (empty($blogPost->slug)) {
+            $blogPost->slug = Str::slug($blogPost->title);
+        }
+    }
+
+    /**
+     * Handle the models blog post "created" event.
+     *
+     * @param BlogPost $blogPost
+     * @return void
+     */
+    public function created(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the models blog post "updated" event.
+     *
+     * @param BlogPost $blogPost
+     * @return void
+     */
+    public function updated(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the models blog post "deleted" event.
+     *
+     * @param BlogPost $blogPost
+     * @return void
+     */
+    public function deleted(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the models blog post "restored" event.
+     *
+     * @param BlogPost $blogPost
+     * @return void
+     */
+    public function restored(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the models blog post "force deleted" event.
+     *
+     * @param BlogPost $blogPost
+     * @return void
+     */
+    public function forceDeleted(BlogPost $blogPost)
+    {
+        //
+    }
+}
